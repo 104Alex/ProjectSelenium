badd9e3a7464d37bffd70b4b67ebffb43351c9bc
Alex <Lexa_7.1989@mail.ru>
2022-04-13 20:16:51 +0300

    Commit

--- src/main/java/Lesson13_14.java
+++ src/main/java/Lesson13_14.java
@@ -1,9 +1,13 @@
 public class Lesson13_14 {
     public static void main(String[] args) {
+        //for (int i = 0; i < 10; i++) {
+           // if (i== 5)
+            //    break;
+            //System.out.println(i);
         for (int i = 0; i < 10; i++) {
-            if (i== 5)
-                break;
-            System.out.println(i);
+         if (i== 5)
+        continue;
+        System.out.println(i);
         }
 
 
