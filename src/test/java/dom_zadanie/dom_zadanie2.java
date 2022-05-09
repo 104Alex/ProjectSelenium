decaf37aa7241359d474e49cb3e1c0fe5efd7520
Alex <Lexa_7.1989@mail.ru>
2022-05-09 23:59:10 +0300

    Commit5

---
+++ src/test/java/dom_zadanie/dom_zadanie2.java
@@ -0,0 +1,25 @@
+package dom_zadanie;
+
+class dom_zadanie {
+    public static void main(String[] args)
+    {
+
+        int a = 69;
+        int b = 11;
+        int c = a + b;
+        int d = a * b;
+        int m = a - b;
+        int n = a / b;
+        int o = a%b;
+
+        System.out.println("Сумма = " + c);
+        System.out.println("Произведение = " + d);
+        System.out.println("Разность = " + m);
+        System.out.println("Частное = " + n);
+        System.out.println("Остаток от деления = " + o);
+
+
+    }
+
+}
+
