3fb7ddbef6e9e2a01754cdd6199ecc106e105b78
Alex <Lexa_7.1989@mail.ru>
2022-05-10 00:12:47 +0300

    Commit5

---
+++ src/test/java/dom_zadanie/dom_zadanie1_2.java
@@ -0,0 +1,22 @@
+package dom_zadanie;
+
+public class dom_zadanie1_2  {
+    public static void main(String[] args) {
+
+        int a = 68;
+        int b = 11;
+        if (a % 2 == 0)
+            System.out.println("Число a  четное");
+        else
+            System.out.println("Число a нечетное ");
+
+        if (b % 2 == 0)
+            System.out.println("Число b  четное");
+        else
+            System.out.println("Число b нечетное ");
+
+
+
+    }
+}
+
