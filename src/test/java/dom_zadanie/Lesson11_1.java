3207a0ff996098408fe3f838113d40fe72ac20f2
Alex <Lexa_7.1989@mail.ru>
2022-05-10 22:56:05 +0300

    Commit5

---
+++ src/test/java/dom_zadanie/Lesson11_1.java
@@ -0,0 +1,34 @@
+package dom_zadanie;
+
+import java.util.Scanner;
+
+
+
+public class Lesson11_1 {
+    public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение: ");
+    int num = in.nextInt();
+
+    switch(num) {
+
+        case 1:
+            System.out.println("Сложение");
+            break;
+        case 2:
+            System.out.println("Вычитание");
+            break;
+        case 3:
+            System.out.println("Умножение");
+            break;
+        default:
+            System.out.println("Операция не определена");
+
+
+            in.close();
+    }
+}
+}
+
+
